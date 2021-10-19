0cef204c36a59b3b6a265e0c2fb44f512c531b9b
zcr003 <zramsey1@cnm.edu>
2021-10-19 11:01:45 -0600

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/dicewareservice/DicewareServiceApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.dicewareservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicewareServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DicewareServiceApplication.class, args);
+  }
+
+}
