d92c82d284cd471d045d04dc207886a9e0835ac2
zcr003 <zramsey1@cnm.edu>
2021-10-20 14:03:42 -0600

    Generated passphrase and thymeleaf.

--- src/main/java/edu/cnm/deepdive/dicewareservice/controller/PassphraseController.java
+++ src/main/java/edu/cnm/deepdive/dicewareservice/controller/PassphraseController.java
@@ -2,12 +2,13 @@ package edu.cnm.deepdive.dicewareservice.controller;
 
 import edu.cnm.deepdive.dicewareservice.service.PassphraseGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/passphrases")
@@ -21,13 +22,20 @@ public class PassphraseController {
       PassphraseGenerator generator) {
     this.generator = generator;
   }
-
+  //This method is returning an array of String. The entire "body."
   @ResponseBody
-  @GetMapping("/transient")
+  @GetMapping(value = "/transient", produces = MediaType.APPLICATION_JSON_VALUE)
   public String[] generate(@RequestParam(defaultValue = "5") int length) {
     return generator.generate(length);
   }
 
+  //Controller method. Return value is the name of the template.
+  @GetMapping(value = "/transient", produces = MediaType.TEXT_HTML_VALUE)
+  public String generate(@RequestParam(defaultValue = "5") int length, Model model) {
+    model.addAttribute("words", generator.generate(length));
+    return "passphrase";
+
+  }
 
 
 }
