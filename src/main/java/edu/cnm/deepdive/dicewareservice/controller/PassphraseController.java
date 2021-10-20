1c4fc0af307a694e7c43b45c7d6e9a1aa2c04fef
zcr003 <zramsey1@cnm.edu>
2021-10-20 12:01:12 -0600

    Generated passphrase.

--- src/main/java/edu/cnm/deepdive/dicewareservice/controller/PassphraseController.java
+++ src/main/java/edu/cnm/deepdive/dicewareservice/controller/PassphraseController.java
@@ -2,12 +2,14 @@ package edu.cnm.deepdive.dicewareservice.controller;
 
 import edu.cnm.deepdive.dicewareservice.service.PassphraseGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/passphrases")
 public class PassphraseController {
 
@@ -20,7 +22,7 @@ public class PassphraseController {
     this.generator = generator;
   }
 
-
+  @ResponseBody
   @GetMapping("/transient")
   public String[] generate(@RequestParam(defaultValue = "5") int length) {
     return generator.generate(length);
