b5b933d384f98b18433355f2ee64a1cc909bd16f
zcr003 <zramsey1@cnm.edu>
2021-10-20 11:43:44 -0600

    Generated passphrase.

--- src/main/java/edu/cnm/deepdive/dicewareservice/controller/PassphraseController.java
+++ src/main/java/edu/cnm/deepdive/dicewareservice/controller/PassphraseController.java
@@ -4,6 +4,7 @@ import edu.cnm.deepdive.dicewareservice.service.PassphraseGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,8 +19,13 @@ public class PassphraseController {
       PassphraseGenerator generator) {
     this.generator = generator;
   }
+
+
   @GetMapping("/transient")
-  public String[] generate(int length) {
+  public String[] generate(@RequestParam(defaultValue = "5") int length) {
     return generator.generate(length);
   }
+
+
+
 }
--- src/main/java/edu/cnm/deepdive/dicewareservice/service/DicewareGenerator.java
+++ src/main/java/edu/cnm/deepdive/dicewareservice/service/DicewareGenerator.java
@@ -1,23 +1,33 @@
 package edu.cnm.deepdive.dicewareservice.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import org.springframework.stereotype.Service;
 
-//This is inheritance. DicewareGenerator implements Passphrasegen. So it essentially is the interface.
+//This is inheritance. DicewareGenerator implements PassphraseGen. So it essentially is the interface.
 @Service
 public class DicewareGenerator implements PassphraseGenerator {
 
   private final List<String> words;
   private final Random rng;
 
-  public DicewareGenerator(List<String> words, Random rng) {
-    this.words = words;
+  //Constructor.
+  public DicewareGenerator(WordListProvider provider, Random rng) {
+    this.words = new ArrayList<>(provider.getWords());
     this.rng = rng;
   }
 
+  //Method to generate the passphrase.
   @Override
   public String[] generate(int length) {
-    return new String[0];
+    String[] passPhrase = new String[length];
+    for (int i = 0; i < length; i++) {
+      passPhrase[i] = words.get(rng.nextInt(words.size()));
+    }
+
+    return passPhrase;
+
+
   }
 }
